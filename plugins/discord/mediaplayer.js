3548e14590f1ed35efebeb8e785cd2096af8d881
dogr95 <rayquasa181@gmail.com>
2020-07-30 20:42:45 +0200

    fixed shuffle

--- plugins/discord/mediaplayer.js
+++ plugins/discord/mediaplayer.js
@@ -3,21 +3,19 @@ module.exports = function mediaplayer(Hahnrich, connection=null, file=null, skip
   if(connection && !skip) {
     Hahnrich.discord.now_playing.set(connection.channel.guild.id, null)
     if(Hahnrich.discord.settings.repeat) {
-      console.debug('repeating')
       const disp = connection.play(file)
       disp.on('finish', () => {
         mediaplayer(Hahnrich, connection, file)
       })
     } else if(Hahnrich.discord.settings.shuffle) {
-      console.debug('shuffling')
       let files = F.readdirSync('plugins/discord/songs')
       // removing current song from selection
-      let name = file.split('/')
-      name = name[name.length-1]
-      files.splice(files.indexOf(file.split('/')[name]), 1)
+      let split = file.split('/')
+      name = split[split.length-1]
+      files.splice(files.indexOf(name), 1)
       // because you dont want to listen to the same song twice while using shuffle lol
       let selection = Math.floor(Math.random() * files.length)
-      console.debug(files, selection, files[selection])
+      console.debug(files, selection, files[selection], name)
       const disp = connection.play(`plugins/discord/songs/${files[selection]}`)
       Hahnrich.discord.now_playing.set(connection.channel.guild.id, files[selection])
       disp.on('finish', () => {
@@ -27,7 +25,6 @@ module.exports = function mediaplayer(Hahnrich, connection=null, file=null, skip
       connection.disconnect()
     }
   } else if(connection && skip) {
-    console.debug('trying to skip')
     let files = F.readdirSync('plugins/discord/songs')
     let selection = files[Math.floor(Math.random() * files.length)]
     mediaplayer(Hahnrich, connection, `plugins/discord/songs/${selection}`)
